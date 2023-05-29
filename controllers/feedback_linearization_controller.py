40dc425becd2e298280ec16ed1eecafffb68035a
opszalek <dawidc4@hotmail.com>
2023-05-29 19:49:29 +0200

    Zad1

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -6,10 +6,23 @@ from .controller import Controller
 class FeedbackLinearizationController(Controller):
     def __init__(self, Tp):
         self.model = ManiuplatorModel(Tp)
+        self.kp = 25
+        self.kd = 60
+        self.kp = np.array([[self.kp, 0], [0, self.kp]])
+        self.kd = np.array([[self.kd, 0], [0, self.kd]])
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         """
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+
+        M = self.model.M(x)
+        C = self.model.C(x)
+        q = x[:2]
+        q_dot = x[2:]
+
+        v = q_r_ddot + self.kd@(q_r_dot - q_dot) + self.kp@(q_r - q)
+        tau= M@v[:,np.newaxis] + C@q_dot[:,np.newaxis]
+
+        return tau
--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -19,10 +19,11 @@ class ManiuplatorModel:
         self.d1 = self.l1 / 2
         self.d2 = self.l2 / 2
 
-
-        self.alpha = self.m1 * self.d1**2 + self.m2 * (self.l1**2 + self.d2**2)  +self.m3 *(self.l1**2 + self.l2**2)+ self.I_1 + self.I_2 + self.I_3
+        self.alpha = self.m1 * self.d1 ** 2 + self.m2 * (self.l1 ** 2 + self.d2 ** 2) + self.m3 * (
+                    self.l1 ** 2 + self.l2 ** 2) + self.I_1 + self.I_2 + self.I_3
         self.beta = self.m2 * self.l1 * self.d2 + self.m3 * self.l1 * self.l2
-        self.gamma = self.m2 * self.d2**2 + self.m3 * self.l2**2 + self.I_2 + self.I_3
+        self.gamma = self.m2 * self.d2 ** 2 + self.m3 * self.l2 ** 2 + self.I_2 + self.I_3
+
     def M(self, x):
         """
         Please implement the calculation of the mass matrix, according to the model derived in the exercise
@@ -42,5 +43,5 @@ class ManiuplatorModel:
         q1, q2, q1_dot, q2_dot = x
 
         C = np.array([[-self.beta * np.sin(q2) * q2_dot, -self.beta * np.sin(q2) * (q1_dot + q2_dot)],
-                        [self.beta * np.sin(q2) * q1_dot, 0]])
+                      [self.beta * np.sin(q2) * q1_dot, 0]])
         return C
